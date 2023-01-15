57dd2a192cc21002b73044ce410c195f32f4389a
lincs <linchengshen@yeah.net>
2023-01-15 20:59:48 +0800

    注意：springmvc中由于存在父子容器，listener会接收到2次ContextRefreshedEvent事件

---
+++ spring-mvc-demo/src/main/java/com/gitlab/springmvc/listener/DefaultApplicationListener.java
@@ -0,0 +1,23 @@
+package com.gitlab.springmvc.listener;
+
+import com.gitlab.springmvc.util.RedisListConsumer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+
+@Component
+public class DefaultApplicationListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        ApplicationContext applicationContext = event.getApplicationContext();
+        if (Objects.isNull(applicationContext.getParent())) {
+            new RedisListConsumer().start();
+        }
+
+    }
+}
